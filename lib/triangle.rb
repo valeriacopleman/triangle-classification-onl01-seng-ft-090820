993d5969b373611a9373e8402c28a31e99b04374
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-25 19:32:38 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,8 @@ class Triangle
   def illegal
     if (@one <= 0) || (@two <= 0) || (@three <= 0)
         raise TriangleError
-    #elsif (@one )
+    elsif (@one + @two <= @three) || (@one + @three <= @two) || (@two + @three <= @one)
+        raise TriangleError
     end
   end
   
